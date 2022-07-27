06ab7fa93dfcf7795a037baf1a18dbceca6b6bfe
codigo-code <panella.dante@gmail.com>
2022-07-27 20:37:46 -0300

    clase 2 repaso

---
+++ clase1_consola/clase1_consola/controller/UserController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using clase1_consola.model;
+
+namespace clase1_consola.controller
+{
+    public class UserController
+    {
+
+        // registrar un usuario
+
+        // validar un usuario
+
+        // listar T0d0s los usuarios
+
+        // borrar un usuario por id
+
+        private IList<User> listUser; // <----
+
+        public UserController()
+        {
+            this.listUser = new List<User>();
+        }
+
+        public string saveUser(User user)
+        {
+            // nos faltaria guardar el usuari
+            this.listUser.Add(user);
+            
+            return "save user";
+        }
+
+        public bool checkUser(User user)
+        {
+            bool rta = false;
+            foreach (var item in this.listUser)
+            {
+                if(item.username.Equals(user.username) && item.password.Equals(user.password))
+                {
+                    rta = true;
+                    break;
+                }
+            }
+            return rta;
+        }
+
+        public IList<User> showAllUser()
+        {
+
+            return this.listUser;
+        }
+    }
+}
+
---
+++ clase1_consola/clase1_consola/view/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using clase1_consola.controller;
+using clase1_consola.model;
+
+namespace clase1_consola.view
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserController uc = new UserController();
+
+            Console.WriteLine("Por favor ingrese el nombre de usuario");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Por favor ingrese el password");
+            string password = Console.ReadLine();
+
+            User u = new User();
+
+            u.username = username;
+            u.password = password;
+
+
+            Console.WriteLine(uc.saveUser(u));
+
+            Console.WriteLine("=====================");
+
+            Console.WriteLine("ingrese el username");
+
+            string user = Console.ReadLine();
+
+            Console.WriteLine("ingrese el password");
+
+            string pass = Console.ReadLine();
+
+            // reutilizo el objeto u ---> e instancion nuevamente el objeto 
+            u = new User();
+
+
+            u.username = user;
+            u.password = pass;
+
+            if (uc.checkUser(u))
+            {
+                Console.WriteLine("Access success");
+            }
+            else
+            {
+                Console.WriteLine("Access Denied");
+            }
+            Console.WriteLine("==========MUESTRO TODOS LOS USUARIOS===========");
+            foreach (var item in uc.showAllUser())
+            {
+                Console.WriteLine( "User: " +item.username + ", Pass:" + item.password);
+            }
+
+            Console.WriteLine("Fin del programa");
+        }
+    }
+}
+
